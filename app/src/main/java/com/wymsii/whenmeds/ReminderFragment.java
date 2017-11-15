17345e7806baccf45d000cae4aae5c64a15928e9
Reuben H Wilson <reuben.wilson@wymsii.com>
2017-11-14 22:07:37 -0600

    Small changes test git

--- app/src/main/java/com/wymsii/whenmeds/ReminderFragment.java
+++ app/src/main/java/com/wymsii/whenmeds/ReminderFragment.java
@@ -58,6 +58,7 @@ public class ReminderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        
         View view = inflater.inflate(R.layout.fragment_reminder_list, container, false);
 
         // Set the adapter
